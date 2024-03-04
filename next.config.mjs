5cb5548e477117e1846dae2d09852287eade0c43
Shun Naka <shunnaka@ShunnoMacBook-Pro.local>
2024-03-04 23:46:36 +0900

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/wsort' : '';
+const assetPrefix = isProd ? '/wsort/' : '';
+
 const nextConfig = {
     output: 'export',
+    basePath,
+    assetPrefix,
 }
 
 export default nextConfig
